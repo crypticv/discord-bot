6b6ecc1ce26b75832aed09eb03d6965ef4169acc
GamingBig <joery@joery.com>
2018-11-04 13:36:41 +0100

    made some things to be implemented

---
+++ commands/index.js
@@ -0,0 +1,72 @@
+const botconfig = require("./botconfig.json");
+const auth = require("./auth.json");
+const Discord = require("discord.js");
+const fs = require("fs");
+const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+
+fs.readdir('./commands/',(err,files)=>{
+
+  if (err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if (jsfile.length <= 0){
+    console.log("could not find any commands");
+    return;
+  }
+
+  jsfile.forEach((f,i) => {
+    let props = require(`./commands/${f}`);
+    console.log(`[*] ${f} loaded.`);
+    //client.commands.set(props.help.name,props);
+  });
+});
+
+const delay = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+
+client.on("ready", () => {
+
+  console.log(`${client.user.username} is online on ${client.guilds.size} servers!`);
+  client.user.setActivity(`the sweet sound of bugs and exceptions... 🎶`, {type:"LISTENING"});
+});
+
+client.on("message", message => {
+  let full_command = message.content.split(" ");
+
+  if(message.author.bot) return;
+  //if(message.channel.type === "dm") return;
+  if (!full_command[0].startsWith(botconfig.prefix)) return;
+
+  let command = full_command[0].split(botconfig.prefix).slice(1).join('');
+  let args = full_command.slice(1);
+
+  let commandFile = client.commands.get(command);
+  if (commandFile) commandFile.run(client, message, args, botconfig);
+
+  
+  client.on('message', message => {
+    if (!message.guild) return;
+
+    if (message.content == '$join') {
+      // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+          .then(connection => { // Connection is an instance of VoiceConnection
+            message.reply('I have successfully connected to the channel!');
+          })
+          .catch(console.log);
+      }else if (message.content === $leave) {
+        if (message.member.voiceChannel) {
+          message.member.voiceChannel.leave()
+          message.channel.send('Goodbye!');
+        }
+      }
+      } else {
+        message.reply('You need to join a voice channel first!');
+    }
+  });
+
+});
+
+client.login(auth.token);
