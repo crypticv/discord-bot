36e2550de4fe323788701027ec1e12cad927b911
GamingBig <joery@joery.com>
2018-11-05 18:19:25 +0100

    updated README.md

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
-module.exports.run = (client, message, args, botconfig) =>{
+module.exports.run = (client, message, args, botconfig) => {
   //if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal! You dont have Admin");
   if(!message.member.hasPermission("ADMINISTRATOR")) {
     return message.reply('You don\'t have permission for this command.\nPlease ask staff if it\'s important.')
