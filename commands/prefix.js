8745b9cffcbf0ef7b4a06a489187041424884fdd
GamingBig <joery@joery.com>
2018-11-05 17:52:15 +0100

    prefix command added

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,14 +1,21 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 
-module.exports.run = async (client, message, args, botconfig) =>{
-  if(message.member.hasPermission("ADMINISTRATOR") {
-    var prefixsel = args.[0];
-    var prefixnow = botconfig.prefix;
-    prefixnow = prefixsel;
-    message.reply(`The prefix is now ${prefixsel}`);
-  } else {message.reply('You don\'t have permission for this command.\nPlease ask staff if it\'s important.')}
+module.exports.run = (client, message, args, botconfig) =>{
+  //if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("No can do pal! You dont have Admin");
+  if(!message.member.hasPermission("ADMINISTRATOR")) {
+    return message.reply('You don\'t have permission for this command.\nPlease ask staff if it\'s important.')
+  }
+  var prefixsel = args[0]
+  botconfig.prefix = prefixsel;
+  prefixnow = prefixsel
+
+  fs.writeFile("../botconfig.json", JSON.stringify(botconfig.prefix), (err) => {
+    if(err) console.log(err)
+  });
+
+  message.reply(`The prefix is now ${prefixsel}`)
 }
-
 module.exports.help = {
   name:"prefix"
 }
